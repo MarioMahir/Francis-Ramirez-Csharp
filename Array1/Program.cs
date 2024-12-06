973f72b3cbaef25f84505b7a80a61e3abbbbbc53
Mario Alejandro Sabala Encarnación <142932859+MarioMahir@users.noreply.github.com>
2024-12-06 10:23:53 -0400

    Array 1
    Escribe un programa que cree una matriz de 20 elementos de tipo entero e inicialice cada uno de los elementos con un valor igual al indice del elemento multiplicado por 5. Imprime los elementos en la consola.

---
+++ Array1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Array1
+{
+    public static void Main(string[] args)
+    {
+        const int TAMANO_MATRIZ = 20; // Tamaño del array
+        int[] matriz = new int[TAMANO_MATRIZ]; // Declarar e inicializar el array
+
+        // Se debe inicializar cada elemento con índice * 5
+        for (int i = 0; i < TAMANO_MATRIZ; i++)
+        {
+            matriz[i] = i * 5;
+        }
+
+        // Mandar a imprimir elementos en la consola
+        Console.WriteLine("Elementos del array:");
+        for (int i = 0; i < TAMANO_MATRIZ; i++)
+        {
+            Console.WriteLine($"Elemento [{i}] = {matriz[i]}");
+        }
+    }
+}

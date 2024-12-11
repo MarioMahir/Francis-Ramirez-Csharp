45c3add99e878c6cfab02029c1c74f21cd53061d
Mario Alejandro Sabala Encarnación <142932859+MarioMahir@users.noreply.github.com>
2024-12-10 22:28:46 -0400

    Clases y objetos - Mario A.

---
+++ Cuenta/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Cuenta
+{
+    private decimal saldo; 
+
+    public Cuenta(decimal saldoInicial)
+    {
+        Saldo = saldoInicial; 
+    }
+
+    public void Acredita(decimal monto)
+    {
+        Saldo = Saldo + monto; 
+    }
+
+    public decimal Saldo
+    {
+        get
+        {
+            return saldo;
+        }
+        set
+        {
+            if (value >= 0)
+                saldo = value;
+        }
+    }
+}
+
+public class PruebaCuenta
+{
+    public static void Main(string[] args)
+    {
+        Cuenta cuenta1 = new Cuenta(50.00M); 
+        Cuenta cuenta2 = new Cuenta(-7.53M); 
+
+        Console.WriteLine("Saldo de cuenta1: {0:C}", cuenta1.Saldo); 
+        Console.WriteLine("Saldo de cuenta2: {0:C}\n", cuenta2.Saldo); 
+
+        decimal montoDeposito; 
+
+        Console.Write("Escriba el monto a depositar para la cuenta1: ");
+        montoDeposito = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Se sumó {0:C} al saldo de cuenta1\n", montoDeposito);
+        cuenta1.Acredita(montoDeposito);
+
+        Console.WriteLine("Saldo de cuenta1: {0:C}", cuenta1.Saldo);
+        Console.WriteLine("Saldo de cuenta2: {0:C}\n", cuenta2.Saldo);
+
+        Console.Write("Escriba el monto a depositar para la cuenta2: ");
+        montoDeposito = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Se sumó {0:C} al saldo de cuenta2\n", montoDeposito);
+        cuenta2.Acredita(montoDeposito); 
+
+        Console.WriteLine("Saldo de cuenta1: {0:C}", cuenta1.Saldo);
+        Console.WriteLine("Saldo de cuenta2: {0:C}", cuenta2.Saldo);
+    }
+}
---
+++ LibroCalificacionesClase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class LibroCalificaciones
+{
+    private string nombreCurso; 
+
+    public LibroCalificaciones(string nombre)
+    {
+        NombreCurso = nombre;
+    }
+
+    public string NombreCurso
+    {
+        get
+        {
+            return nombreCurso;
+        }
+        set
+        {
+            nombreCurso = value;
+        }
+    }
+
+    public void MostrarMensaje()
+    {
+        Console.WriteLine("Bienvenido al libro de calificaciones para\n{0}!", NombreCurso);
+    }
+}
+
+public class PruebaLibroCalificaciones
+{
+    public static void Main(string[] args)
+    {
+        LibroCalificaciones libroCalificaciones1 = new LibroCalificaciones("CS101 Introducción a la programación en C#");
+        LibroCalificaciones libroCalificaciones2 = new LibroCalificaciones("CS102 Estructuras de datos en C#");
+
+        Console.WriteLine("El nombre del curso de libroCalificaciones1 es: {0}", libroCalificaciones1.NombreCurso);
+        Console.WriteLine("El nombre del curso de libroCalificaciones2 es: {0}", libroCalificaciones2.NombreCurso);
+    }
+}
